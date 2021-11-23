0fa65890321ce9929f89c0a975b6442013c56ad4
euguroglu <e.uguroglu@yahoo.com>
2021-11-23 20:15:37 +0300

    Cache clearing method added

--- src/main/java/com/enesuguroglu/Controller/RedisCacheController.java
+++ src/main/java/com/enesuguroglu/Controller/RedisCacheController.java
@@ -10,11 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class RedisCacheController {
 
+    private int counter = 0;
+
     @Autowired
     private RedisCacheService redisCacheService;
 
     @GetMapping
     public String cacheControl() throws InterruptedException{
+        if (counter == 5){
+            redisCacheService.clearCache();
+            counter = 0;
+        }
+        counter++;
         return redisCacheService.longRunningMethod();
     }
 
--- src/main/java/com/enesuguroglu/Service/RedisCacheService.java
+++ src/main/java/com/enesuguroglu/Service/RedisCacheService.java
@@ -1,14 +1,22 @@
 package com.enesuguroglu.Service;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisCacheService {
 
+    //Simple example method for cache service
+    //After succesfull caching we wont see 5 second sleep time during api response
     @Cacheable(cacheNames = "Cache1")
     public String longRunningMethod() throws InterruptedException{
         Thread.sleep(5000L);
         return "method worked";
+    }
+    //Method to clear cache
+    @CacheEvict(cacheNames = "Cache1")
+    public void clearCache(){
+        System.out.println("cache cleared");
     }
 }
